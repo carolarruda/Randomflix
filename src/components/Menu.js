f3482a29ef50678077656095bd0bf426f1e42483
carolina arruda <carolinacalouroarruda@gmail.com>
2023-09-28 15:45:58 +0200

    working on top nav

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import logo from "../images/RandomflixDark.png";
+import classes from "./Menu.module.scss";
+
+const Menu = () => {
+  return (
+    <div className={classes.container}>
+      <img className={classes.logo} src={logo} alt="logo" />
+      <div className={classes.searchContainer}>
+
+        <input type="text" className={classes.searchHover} name="" placeholder="search here..." />
+
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
