e9c2de5f5cb20a47e2796248cf574f9795e72818
carolina arruda <carolinacalouroarruda@gmail.com>
2023-10-02 19:11:20 +0200

    working on main grid fetch

--- src/api/index.js
+++ src/api/index.js
@@ -10,12 +10,13 @@ const apiKey = "c36894549172755979bb9bdfab24093e";
 const params = {
   api_key: apiKey,
   language: "en-US",
+
 };
 
 const get = (url, query) =>
   instance.get(url, {
     params: {
-
+      ...query,
       ...params,
     },
   });
--- src/components/Main/MovieList/MovieList.js
+++ src/components/Main/MovieList/MovieList.js
@@ -4,36 +4,68 @@ import { get } from "../../../api/index";
 
 const MovieList = () => {
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchMovies() {
+    try {
+      setLoading(true);
+      const response = await get("popular", { page });
+      setData((prevData) => [...prevData, ...response.data.results]);
+      setPage((prevPage) => prevPage + 1); 
+console.log(data);
+    } catch (error) {
+      console.error("Error fetching popular movies:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   useEffect(() => {
-    async function fetchMovies() {
-      try {
-        const response = await get("popular");
-        setData(response.data.results);
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching popular movies:", error);
-      }
-    }
-
     fetchMovies();
-  }, [data]);
+  }, []);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        fetchMovies();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); 
 
   return (
-    <div>
+    <section className={classes.main}>
       <h1>Popular Movies</h1>
       <div className={classes.movieGrid}>
-        {data.length > 0 &&
-          data.map((movie) => (
-            <div className={classes.poster}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                alt="img"
-              />
+        {data.map((movie) => (
+          <div key={movie.id} className={classes.poster}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt="img"
+            />
+            <div>
+              <h2>{movie.title}</h2>
+              <h3>{movie.release_date.slice(0, 4)}</h3>
+              <span>{movie.vote_average}</span>
+              <div>
+           
+              </div>
             </div>
-          ))}
+          </div>
+        ))}
+        {loading && <p>Loading...</p>}
       </div>
-    </div>
+    </section>
   );
 };
 
