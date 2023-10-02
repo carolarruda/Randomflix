7574c14a831008cf94818a27177cdcdc07f141f1
carolina arruda <carolinacalouroarruda@gmail.com>
2023-10-02 16:16:23 +0200

    added grid with movie posters

--- src/components/Main/MovieList/MovieList.js
+++ src/components/Main/MovieList/MovieList.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import {get} from '../../../api/index'
+import React, { useEffect, useState } from "react";
+import classes from "./MovieList.module.scss";
+import { get } from "../../../api/index";
 
 const MovieList = () => {
   const [data, setData] = useState([]);
@@ -7,25 +8,31 @@ const MovieList = () => {
   useEffect(() => {
     async function fetchMovies() {
       try {
-        const response = await get(
-          'popular',
-        );
+        const response = await get("popular");
         setData(response.data.results);
+        console.log(data);
       } catch (error) {
-        console.error('Error fetching popular movies:', error);
+        console.error("Error fetching popular movies:", error);
       }
     }
 
     fetchMovies();
-  }, []);
+  }, [data]);
 
   return (
     <div>
       <h1>Popular Movies</h1>
-      <ul>
+      <div className={classes.movieGrid}>
         {data.length > 0 &&
-          data.map((movie) => <li key={movie.id}>{movie.title}</li>)}
-      </ul>
+          data.map((movie) => (
+            <div className={classes.poster}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt="img"
+              />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
